17b9c7957e34faec28b296993933fe471023d507
hersson ceballos <yerssonc@gmail.com>
2020-03-24 01:07:11 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,4 @@
 # Build your say_hello method here
-  it 'defaults to your name Ruby Programmer when no name is passed in' do
-  it 'defaults to Ruby Programmer when no name is passed in' do
-    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
-    say_hello()
-  end
-
+ 
 
 
